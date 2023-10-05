5a7e65029b0956621f0024a259a10ec67598d23a
Silvio P C Filho <110847897+Silviop301@users.noreply.github.com>
2023-10-05 18:25:24 -0300

    Update poke-api.js

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -5,7 +5,19 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
+    pokemon.abilities = pokeDetail.abilities.map(ability => ({
+        name: ability.ability.name,
+        url: ability.ability.url,
+        isHidden: ability.is_hidden,
+        slot: ability.slot
+    }));
 
+    pokemon.stats = pokeDetail.stats.map(stat => ({
+        baseStat: stat.base_stat,
+        name: stat.stat.name,
+        url: stat.stat.url
+    }));
+    
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
@@ -32,4 +44,10 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
+}
+pokeApi.getPokemonDetailByName = (name) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+    return fetch(url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon);
 }
